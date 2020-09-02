4c9a445e8cc9f9920e1c79d8e760460b21aaa529
HiPeng-sange <51777997+HiPeng-sange@users.noreply.github.com>
2020-09-02 21:43:28 +0700

    First commit

---
+++ app/src/main/java/com/losdol/prm391x_shopmovies_hidayatmhyfx07085/MainActivity.java
@@ -0,0 +1,14 @@
+package com.losdol.prm391x_shopmovies_hidayatmhyfx07085;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
