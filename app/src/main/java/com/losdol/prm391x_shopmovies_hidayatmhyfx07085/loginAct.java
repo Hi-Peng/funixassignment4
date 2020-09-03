c3ee80bbee39860f53b6150a360f0545c0573d04
HiPeng-sange <51777997+HiPeng-sange@users.noreply.github.com>
2020-09-03 08:16:45 +0700

    adding G-Auth

--- app/src/main/java/com/losdol/prm391x_shopmovies_hidayatmhyfx07085/loginAct.java
+++ app/src/main/java/com/losdol/prm391x_shopmovies_hidayatmhyfx07085/loginAct.java
@@ -1,21 +1,30 @@
 package com.losdol.prm391x_shopmovies_hidayatmhyfx07085;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 import java.util.Arrays;
 
 public class loginAct extends AppCompatActivity {
+    private static String TAG = "Facebook Login";
+
     CallbackManager callbackManager;
     LoginButton loginButton;
+
 
     private static final String EMAIL = "email";
     @Override
@@ -23,6 +32,11 @@ public class loginAct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         callbackManager = CallbackManager.Factory.create();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
 
         //This call back code i got from official facebook dev documentation
         loginButton = (LoginButton) findViewById(R.id.login_button);
@@ -33,20 +47,26 @@ public class loginAct extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Intent i = new Intent(loginAct.this, mainAct.class);
-                startActivity(i);
+                startActivity(new Intent(loginAct.this, mainAct.class));
+                Log.d(TAG, "onSuccess: " + "Login Success, starting new activity");
             }
 
             @Override
             public void onCancel() {
-                // App code
+                Log.d(TAG, "onCancel: " + "Login Canceled");
             }
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
+                Log.e(TAG, "onError: Error Login", exception);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        startActivity(new Intent(loginAct.this, mainAct.class));
     }
 }
--- app/src/main/java/com/losdol/prm391x_shopmovies_hidayatmhyfx07085/mainAct.java
+++ app/src/main/java/com/losdol/prm391x_shopmovies_hidayatmhyfx07085/mainAct.java
@@ -1,4 +1,14 @@
 package com.losdol.prm391x_shopmovies_hidayatmhyfx07085;
 
-public class mainAct {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainact);
+    }
 }
