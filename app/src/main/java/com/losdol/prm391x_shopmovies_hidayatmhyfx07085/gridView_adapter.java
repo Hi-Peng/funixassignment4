630163e6ee1b6dd3f8c99348e996dbe6d76b2e82
HiPeng-sange <51777997+HiPeng-sange@users.noreply.github.com>
2020-09-03 15:57:50 +0700

    Done with the gridView

--- app/src/main/java/com/losdol/prm391x_shopmovies_hidayatmhyfx07085/gridView_adapter.java
+++ app/src/main/java/com/losdol/prm391x_shopmovies_hidayatmhyfx07085/gridView_adapter.java
@@ -4,15 +4,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 import com.squareup.picasso.Picasso;
 
@@ -21,50 +25,49 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.zip.Inflater;
 
 import static androidx.constraintlayout.motion.widget.MotionScene.TAG;
 
 public class gridView_adapter extends BaseAdapter {
-    private Context mContext;
-    private ArrayList<HashMap<String, String>> movieList;
+    private Context context;
+    private ArrayList<HashMap<String, String>> moviesList;
+    LayoutInflater inflater;
 
-    public gridView_adapter(Context mContext, ArrayList<HashMap<String, String>> movieList) {
-        this.mContext = mContext;
-        this.movieList = movieList;
+    public gridView_adapter(Context context, ArrayList<HashMap<String, String>> movList) {
+        this.inflater=LayoutInflater.from(context);
+        this.moviesList = movList;
     }
 
     @Override
     public int getCount() {
-        return movieList.size();
+        return moviesList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return movieList.get(position);
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
-    //Here's my custom row adapter, the coding style soo bad, next time i'll be naming better instance/object
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater layoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View customRow =  layoutInflater.inflate(R.layout.gv_adapter, parent, false);
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            view = inflater.inflate(R.layout.gv_adapter, viewGroup, false);
+            TextView priceText = view.findViewById(R.id.price);
+            TextView movieText = view.findViewById(R.id.mov_name);
+            ImageView imgIV = view.findViewById(R.id.imageView);
 
-        TextView textViewItem = customRow.findViewById(R.id.mov_name);
-        TextView priceViewItem = customRow.findViewById(R.id.price);
-        ImageView imageViewItem = customRow.findViewById(R.id.imageView);
+            HashMap<String, String> mapMovies = (HashMap<String, String>) moviesList.get(position);
 
-        HashMap<String, String> mapMovies = (HashMap<String, String>) movieList.get(position);
-        Log.d(TAG, "getView: " + mapMovies.get("image"));
-
-        Picasso.get().load(mapMovies.get("image")).into(imageViewItem);
-        priceViewItem.setText(mapMovies.get("price"));
-        textViewItem.setText(mapMovies.get("title"));
-        return convertView;
-
+            Picasso.get().load(mapMovies.get("image")).into(imgIV);
+            priceText.setText(mapMovies.get("price"));
+            movieText.setText(mapMovies.get("title"));
+        }
+        return view;
     }
 }
